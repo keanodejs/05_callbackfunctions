d1c6b33cdd1d862602c93098c8df8f76ac13dd22
Claus Bové <clbo@kea.dk>
2016-03-02 11:32:14 +0100

    initial commit

---
+++ node_demo_asynchronous.js
@@ -0,0 +1,9 @@
+// An asynchronous example using a callback function
+
+fs = require('fs');
+
+fs.readdir('/Users/clbo/Documents/test', function(err, data) {
+    console.log('data:', data);
+});
+
+console.log("this comes after");
